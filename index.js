ee7d03ce75784b2e15dd4ad3cbc75abf0fe8935f
neela <nusratneela27@gmail.com>
2023-06-12 18:43:33 +0600

    user api added

--- index.js
+++ index.js
@@ -52,7 +52,19 @@ async function run() {
             res.send(result);
         })
 
-        // ClassCartCollection
+        // ClassCart Collection API
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            if (!email) {
+                res.send([]);
+            }
+
+            const query = { email: email };
+            const result = await classCartCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post('/carts', async (req, res) => {
             const selectedClass = req.body;
             const result = await classCartCollection.insertOne(selectedClass);
