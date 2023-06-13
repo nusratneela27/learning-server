3c54d647bace981b78053a862b6d476b5d0dfd8b
neela <nusratneela27@gmail.com>
2023-06-13 16:38:13 +0600

    jwt added

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.imrvi6v.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,8 +46,17 @@ async function run() {
         const classCollection = client.db("learningCamp").collection("class");
         const classCartCollection = client.db("learningCamp").collection("classCart");
 
+        // jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ token })
+        })
+
         // users related API
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -48,7 +73,7 @@ async function run() {
             res.send(result);
         })
 
-        // admin
+        // admin update
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
@@ -62,7 +87,7 @@ async function run() {
             res.send(result)
         })
 
-        // instructor
+        // instructor update
         app.patch('/users/instructor/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
@@ -92,11 +117,16 @@ async function run() {
         })
 
         // ClassCart Collection API
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             console.log(email);
             if (!email) {
                 res.send([]);
+            }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
             }
 
             const query = { email: email };
